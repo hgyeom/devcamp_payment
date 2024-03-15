467e3c68fe4ed87d569bbde9152ccaa5fe3b38bf
hgyeom <rrune3@naver.com>
2024-03-15 09:59:49 +0900

    remove : fail 페이지 삭제

--- src/app/fail/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function FailPage() {
-  return (
-    <main
-      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
-    >
-      <h1>결제 실패</h1>
-    </main>
-  );
-}
